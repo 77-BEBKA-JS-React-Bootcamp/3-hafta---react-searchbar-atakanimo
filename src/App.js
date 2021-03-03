6d75296436e9864b7f672114100497316fa69266
AtakanAtabeyoglu <70342818+atakanimo@users.noreply.github.com>
2021-03-03 18:07:48 +0300

    FindCountry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.scss";
+import CardUI from './Card/Card';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input
+          type="text"
+          className="search"
+          placeholder="Type a Country!"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {search === "" ? (
+          <p style={{fontSize: "26px",textAlign:"center"}} class="no-search">Fill the input to See Country List!</p>
+        ) : (
+            <div>
+              {data
+                .filter(
+                  (item) =>
+                    item.name
+                      .toLowerCase()
+                      .includes(search.toLowerCase())
+                )
+                .map((item, i) => (
+
+                  <CardUI
+                    flag={item.flag}
+                    name={item.name}
+                    code={item.alpha2Code}
+                    capital={item.capital}
+                    region={item.region}
+                  />
+
+                ))}
+            </div>
+
+          )}
+      </form>
     </div>
   );
 }
 
 export default App;
+
